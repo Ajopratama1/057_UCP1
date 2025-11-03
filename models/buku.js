c23130038dd080d72ff64ab57c89e27bb9f2840a
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-03 09:19:43 +0700

    membuat table buku

---
+++ models/buku.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Buku = sequelize.define('Buku', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    judul: {
+      type: DataTypes.STRING,
+    },
+    pengarang: {
+        type: DataTypes.STRING,
+    },
+    tahun_terbit: {
+      type: DataTypes.STRING,
+    },
+    bidang:{
+        type: DataTypes.STRING,
+    }
+  });
+
+  return Buku;
+};
